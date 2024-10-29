e27e0f55b7bb6f781c285f5d7f9e9241be45ced2
GeorKhabalaev228007 <geo20062006@gmail.com>
2024-10-29 11:57:18 +0700

    Add files via upload

---
+++ lab4.c/lab4-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int c = 1;
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%d ", c++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+// int main(){
+//     int n = 3;
+//     int b = 1;
+//     for(int i = 0; n > i; i++){
+//         for(int x =)
+//     }
+// }
